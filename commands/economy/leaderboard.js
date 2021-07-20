d1d4cd6ec41373e1c42223a69664e255429bcb30
Danchikkk <46873266+Danchikkk@users.noreply.github.com>
2021-07-20 10:59:57 +0400

    Add files via upload

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -17,30 +17,31 @@ module.exports = {
     },
     run: async (bot, message, args) => {
     let msg = await message.channel.send("**Секунду**");
-    const users = await profileModel.find({serverID: message.guild.id})
-    const userss = await profileModel.find()
+    const users = await profileModel.find()
 
-    const lbb = userss
-              .slice(0)
-              .sort(({ bank: a }, { bank: b }) => b - a)
-              .map(
-
-                ({ userID, bank }, pos) => `__${pos + 1}.__ <@${userID}> - **${commaNumber(bank)}** ${COIN}`,
-              );
     const lb = users
               .slice(0)
               .sort(({ bank: a }, { bank: b }) => b - a)
+              .slice(0, 15)
+              .filter(
+                function({userID}) {
+                  let guild = bot.guilds.cache.get(message.guild.id)
+
+                  return guild.member(userID)
+
+                }
+              )
               .map(
 
-                ({ userID, bank }, pos) => `__${pos + 1}.__ <@${userID}> - **${commaNumber(bank)}** ${COIN}`,
-              );
+                ({ userID, bank }, pos) => `__${pos + 1}.__ <@${userID}> - **${commaNumber(bank)}** ${COIN}`
+              )
+              
 
-          const newnew =     lb.slice(0, 10)
-            const news =     lb.slice(0, 15)
+
+          const newnew =     lb.slice(0, 15)
             const embed = new MessageEmbed()
-              .setTitle('Самые богатые участники по Банку - Top 10')
+              .setTitle('Самые богатые участники по Банку - Top 15')
               .setDescription(newnew)
-              .addField("Глобальный топ по Банку - Top 15", news, true)
               .setColor('RANDOM')
     message.channel.send(embed)
     msg.delete()
