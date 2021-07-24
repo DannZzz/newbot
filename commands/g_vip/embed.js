beac7deddb35e6f943caa49e2650ffc799126dc1
Danchikkk <46873266+Danchikkk@users.noreply.github.com>
2021-07-24 13:00:40 +0400

    Add files via upload

--- commands/g_vip/embed.js
+++ commands/g_vip/embed.js
@@ -8,12 +8,12 @@ const vipModel = require("../../models/vipSchema");
 
 module.exports = {
   config: {
-    name: "канал",
-    description: "Отправлять сообщение на указанном канале.",
+    name: "эмбед",
+    description: "Отправлять сообщение на указанном канале в формате EMBED.",
     category: "g_vip",
-    aliases: ["channel"],
+    aliases: ["embed"],
     accessableby: "Для всех",
-    usage: "[упоминание канала] [ваше сообщение]"
+    usage: "[упоминание канала] [цвет] [ваше сообщение]"
   },
   run: async (bot, message, args) => {
     let embed = new MessageEmbed()
@@ -32,13 +32,18 @@ module.exports = {
     let channel = message.mentions.channels.first();
     if(bag["vip1"] === false) return message.channel.send(embed.setDescription("❌ Эта команда доступна только для **VIP 1** пользователей.")).then(msg => {msg.delete({timeout: "10000"})});
 
-    let arg = args.slice(1).join(" ")
+    let arg = args.slice(2).join(" ")
+
     if(!args[0]) return message.channel.send(embed.setDescription("❌ Укажите #текстовый канал.")).then(msg => {msg.delete({timeout: "10000"})});
+    if(!args[1]) return message.channel.send(embed.setDescription("❌ Укажите цвет эмбед.")).then(msg => {msg.delete({timeout: "10000"})});
     if(!arg) return message.channel.send(embed.setDescription("❌ Укажите текст.")).then(msg => {msg.delete({timeout: "10000"})});
     if(!channel) return message.channel.send(embed.setDescription("❌ Укажите доступный #текстовый канал.")).then(msg => {msg.delete({timeout: "10000"})});
+    let doEmbed = new MessageEmbed()
+    .setColor(`${args[1]}`)
+    .setTimestamp()
+    .setDescription(arg)
 
-
-    channel.send(arg)
+    channel.send(doEmbed)
 
 
   }
