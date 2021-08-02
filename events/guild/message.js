b6a82f10f81b3f3495f6d4679cecc251e366162b
Danchikkk <46873266+Danchikkk@users.noreply.github.com>
2021-08-02 19:10:13 +0400

    Add files via upload

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,10 +1,12 @@
-
+const embed = require('../../embedConstructor');
 const { PREFIX } = require('../../config');
+const { MessageEmbed } = require('discord.js')
 const profileModel = require("../../models/profileSchema");
 const serverModel = require("../../models/serverSchema");
 const memberModel = require("../../models/memberSchema");
 const begModel = require("../../models/begSchema");
 const vipModel = require("../../models/vipSchema");
+const customModel = require("../../models/customSchema");
 const queue2 = new Map();
 const queue3 = new Map();
 const queue = new Map();
@@ -59,8 +61,49 @@ module.exports = async (bot, message) => {
             serverID: message.guild.id,
           })
         server.save()}
-
+        let mes = [
+          'привет',
+          'хай',
+          'всем привет',
+          'Всем привет',
+          'ку',
+          'Хай',
+          'Привет',
+          'Приветик',
+          'приветик',
+          'Ку всем',
+          'Ку',
+          'куу',
+          'Куу',
+          'куку',
+          'Куку',
+          'КуКу',
+          'Хай всем',
+          'доброе утро',
+          'привяю',
+          'Привяю',
+          'Доброе утро'
+        ]
+        let res = [
+          'Заебал',
+          'Куу',
+          'Куку',
+          'Привет',
+          'Хай',
+          'хватит спамить..',
+          'Та привет',
+          'ЭХ недоум..',
+          'Я думал это я бот',
+          'Ку ',
+          'Приветик',
+          'ляяяяяяя',
+          'Пока',
+          "МДээ",
+          'Гыыыы ку-ку'
+        ]
         prefix = serverData.prefix;
+        let rand = Math.floor(Math.random() * res.length)
+        if(mes.includes(message.content)) message.reply(res[rand])
 
         let args = message.content.slice(prefix.length).trim().split(/ +/g);
         let cmd = args.shift().toLowerCase();
@@ -73,9 +116,15 @@ module.exports = async (bot, message) => {
             queue3: queue3,
             games: games
         }
-
+        let ss = new MessageEmbed().setColor("#2f3136").setTimestamp()
         var commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
-        if (commandfile) commandfile.run(bot, message, args, ops, profileData)
+        if (commandfile) {commandfile.run(bot, message, args, ops, profileData)}
+        else {await customModel.findOne({serverID: message.guild.id, command: cmd}, async(err, data) =>{
+        if(err) throw error
+        if(data) return message.channel.send(ss.setDescription(data.content));
+        else return
+        }
+        )}
     } catch (e) {
         console.log(e);
     }
