caaaaf1f8dedb367f9a86d8b7c99cf3f3609602b
Danchikkk <46873266+Danchikkk@users.noreply.github.com>
2021-08-04 00:04:57 +0400

    Add files via upload

--- commands/a_moderation/lock.js
+++ commands/a_moderation/lock.js
@@ -8,7 +8,7 @@ module.exports = {
   config: {
     name: "закрыть",
     description: "Закрывать канал.",
-    usage: "",
+    usage: "(название текстового канала | упоминание | ID)",
     category: "a_moderation",
     accessableby: "Нужна права: Администрация.",
     aliases: ["lock"]
@@ -16,18 +16,19 @@ module.exports = {
   run: async (bot, message, args) => {
     if (!message.member.hasPermission("ADMINISTRATOR")) return embed(message).setError("У вас недостаточно прав.").send().then(msg => {msg.delete({timeout: "10000"})});
     if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) return embed(message).setError("У меня недостаточно прав.").send().then(msg => {msg.delete({timeout: "10000"})});
+    let channel = message.mentions.channels.first() || bot.guilds.cache.get(message.guild.id).channels.cache.get(args[0]) || message.guild.channels.cache.find(c => c.name.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.channel;
 
-    let ow = message.channel.permissionsFor(message.channel.guild.roles.everyone).has("SEND_MESSAGES")
-    if(ow === false) return embed(message).setError(`${message.channel} и так закрыт!`).send().then(msg => {msg.delete({timeout: "10000"})});
+    let ow = channel.permissionsFor(channel.guild.roles.everyone).has("SEND_MESSAGES")
+    if(ow === false) return embed(message).setError(`${channel} и так закрыт!`).send().then(msg => {msg.delete({timeout: "10000"})});
 
-    await message.channel.updateOverwrite(message.channel.guild.roles.everyone, {
+    await channel.updateOverwrite(message.channel.guild.roles.everyone, {
       SEND_MESSAGES: false
     })
     const sembed = new MessageEmbed()
         .setColor(greenlight)
         .setTimestamp()
         .setAuthor(message.guild.name, message.guild.iconURL())
-        .setDescription(`${AGREE} ${message.channel} успешно закрыт.`)
+        .setDescription(`${AGREE} ${channel} успешно закрыт.`)
 
     message.channel.send(sembed)
   }
--- commands/a_moderation/unlock.js
+++ commands/a_moderation/unlock.js
@@ -7,32 +7,28 @@ module.exports = {
   config: {
     name: "открыть",
     description: "Открывать канал.",
-    usage: "",
+    usage: "(название текстового канала | упоминание | ID)",
     category: "a_moderation",
     accessableby: "Нужна права: Администрация.",
     aliases: ["unlock"]
   },
   run: async (bot, message, args) => {
-    let lockEmbed = new MessageEmbed()
-    .setTimestamp()
-    .setAuthor(message.member.user.tag, message.member.user.displayAvatarURL({dynamic: true}))
-    .setColor(redlight)
-
     if (!message.member.hasPermission("ADMINISTRATOR")) return embed(message).setError("У вас недостаточно прав.").send().then(msg => {msg.delete({timeout: "10000"})});
     if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) return embed(message).setError("У меня недостаточно прав.").send().then(msg => {msg.delete({timeout: "10000"})});
+    let channel = message.mentions.channels.first() || bot.guilds.cache.get(message.guild.id).channels.cache.get(args[0]) || message.guild.channels.cache.find(c => c.name.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.channel;
 
-    let ow = message.channel.permissionsFor(message.channel.guild.roles.everyone).has("SEND_MESSAGES")
-    if(ow === true || ow === null) return embed(message).setError(`${message.channel} и так открыт!`).send().then(msg => {msg.delete({timeout: "10000"})});
+    let ow = channel.permissionsFor(channel.guild.roles.everyone).has("SEND_MESSAGES")
+    if(ow === true || ow === null) return embed(message).setError(`${channel} и так открыт!`).send().then(msg => {msg.delete({timeout: "10000"})});
 
 
-    await message.channel.updateOverwrite(message.channel.guild.roles.everyone, {
+    await channel.updateOverwrite(message.channel.guild.roles.everyone, {
       SEND_MESSAGES: null
     })
     const sembed = new MessageEmbed()
         .setColor(greenlight)
         .setTimestamp()
         .setAuthor(message.guild.name, message.guild.iconURL())
-        .setDescription(`${AGREE} ${message.channel} успешно открыт.`)
+        .setDescription(`${AGREE} ${channel} успешно открыт.`)
 
     message.channel.send(sembed)
   }
