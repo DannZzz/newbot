043ad673191be26bf15ce962f59e6b473e3dac17
Danchikkk <46873266+Danchikkk@users.noreply.github.com>
2021-08-07 00:37:00 +0400

    Add files via upload

--- commands/c_economy/roulette.js
+++ commands/c_economy/roulette.js
@@ -4,7 +4,7 @@ const embed = require('../../embedConstructor');
 const profileModel = require("../../models/profileSchema")
 const begModel = require("../../models/begSchema")
 let Roulette = require('../../constructors/Roulette')
-
+const {COIN, BANK} = require('../../config');
 const { addUser, game } = Roulette
 
 module.exports = {
@@ -17,10 +17,13 @@ module.exports = {
         usage: "[ставка] [1-12, 13-24, 25-32]"
   },
   run: async (bot, message, args) => {
-  const types = ['1-12', '13-24', '25-32']
+  const types = ['1-12', '13-24', '25-32'];
+  const colors = ['red', 'black', 'красный', 'черный', 'чёрный'];
+  const numberss = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+  const numbers = numberss.map(rep => rep + '')
   let data = await profileModel.findOne({userID: message.author.id  })
   let beg = await begModel.findOne({ userID: message.author.id });
-  var value = Math.floor(parseInt(args[0]))
+  var value = args[0]
   let author = data.roulette;
   let timeout;
   if (beg["vip2"] === true) { timeout = 31 * 1000; } else {
@@ -33,9 +36,22 @@ module.exports = {
       return embed(message).setError(`Попробуй еще раз через **${time.getSeconds()} секунд.**.`).send().then(msg => {msg.delete({timeout: "10000"})});
   }
 
-  if (!value) return embed(message)
+
+if (value === 'help' || value === 'хелп') {
+    return embed(message)
+          .setPrimary(`**\`\`?рулетка <ставка> [промежуток]\`\`**\n\nВсе доступные промежутки.\n\`\`\`Цвета: красный, черный\nГруппы: 1-12, 13-24, 25-32\nПрямые ставки: 0, 1, 2 ... 32\`\`\``)
+          .send()
+  }
+
+
+  if (!Math.floor(parseInt(value))) {return embed(message)
         .setError('Вы неверно указали сумму.')
-        .send()
+        .send().then(msg => {
+          msg.delete({
+            timeout: "10000"
+          })
+        })
+      }
 
   if (value < 100) return embed(message).setError(`Минимальная ставка **100**.`).send().then(msg => {msg.delete({timeout: "10000"})});
 
@@ -58,21 +74,81 @@ module.exports = {
   if (data.coins < value) return embed(message)
       .setError('У вас недостаточно монет.')
       .send()
+      .then(msg => {
+        msg.delete({
+          timeout: "10000"
+        })
+      })
 
-  if (!types.includes(type)) return embed(message)
-      .setError(`Вы неверно указали промежутки. Доступные промежутки ${types.join()}.`)
+  if (!types.includes(type) && !colors.includes(type) && !numbers.includes(type)) return embed(message)
+      .setError(`Вы неверно указали промежутки.\n\`\`?рулетка хелп\`\``)
       .send()
+      .then(msg => {
+        msg.delete({
+          timeout: "10000"
+        })
+      })
 
   await profileModel.findOneAndUpdate({ userID: message.author.id }, {
       $inc: { coins: -value }
     })
-
-  addUser(message.author.id, type, value, message.channel)
-
-    embed(message)
+    await embed(message)
         .setSuccess('Вы участвуете в рулетке.')
         .send()
 
   await profileModel.findOneAndUpdate({userID: message.author.id}, {$set: {roulette: Date.now()}})
+
+  let randNum = Math.floor(Math.random() * 32)
+  let final;
+  let finalColor;
+  if (randNum >= 1 && randNum <= 12) final = types[0]
+  if (randNum >= 13 && randNum <= 24) final = types[1]
+  if (randNum >= 25 && randNum <= 32) final = types[2]
+  if (randNum <= 16) {finalColor = 'Красный'}
+  else if (randNum <= 32) {
+    finalColor = 'Чёрный'
+  }
+
+  let finalType;
+  let winType;
+  if(types.includes(type)) {
+    finalType = type;
+    winType = 1;
+  } else if (colors.includes(type)) {
+    if(type === 'red' || type === 'красный') {
+      finalType = 'Красный';
+    } else if (type === 'black' || type === 'черный' || type === 'чёрный') {
+      finalType = 'Чёрный';
+    }
+    winType = 2;
+  } else if (numbers.includes(type)) {
+    finalType = type;
+    winType = 3;
+  }
+  setTimeout(async function() {
+    if (winType === 1) {
+      if (finalType === final) {
+        await profileModel.findOneAndUpdate({userID: message.author.id}, {$inc: {coins: value * 3}})
+
+        return embed(message).setSuccess(`Выпало: **${finalColor} ${randNum}**\n\nВы выиграли **${value * 3}** ${COIN}`).send()
+      }
+    } else if (winType === 2) {
+      if (finalType === finalColor) {
+        await profileModel.findOneAndUpdate({userID: message.author.id}, {$inc: {coins: value * 2}})
+
+        return embed(message).setSuccess(`Выпало: **${finalColor} ${randNum}**\n\nВы выиграли **${value * 2}** ${COIN}`).send()
+      }
+    } else if (winType === 3) {
+      if (finalType === randNum) {
+        await profileModel.findOneAndUpdate({userID: message.author.id}, {$inc: {coins: value * 16}})
+
+        return embed(message).setSuccess(`Выпало: **${finalColor} ${randNum}**\n\nВы выиграли **${value * 16}** ${COIN}`).send()
+      }
+    }
+    return embed(message).setError(`Выпало: **${finalColor} ${randNum}**\n\nВы проиграли.`).send()
+
+
+  }, 30 * 1000)
+
   }
 }
