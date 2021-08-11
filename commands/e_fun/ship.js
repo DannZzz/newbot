e900933872d81313951bd00091af66e1d7f349a0
Danchikkk <46873266+Danchikkk@users.noreply.github.com>
2021-08-11 12:06:05 +0400

    Add files via upload

--- commands/e_fun/ship.js
+++ commands/e_fun/ship.js
@@ -1,6 +1,8 @@
 const {MessageEmbed} = require("discord.js");
 const {greenlight, redlight, cyan} = require('../../JSON/colours.json');
 const progressbar = require('string-progressbar');
+const embed = require('../../embedConstructor');
+const {progressBar} = require("../../functions.js")
 
 module.exports = {
     config: {
@@ -12,35 +14,16 @@ module.exports = {
         accessableby: "Для всех"
     },
     run: async (bot, message, args) => {
+      let rand = Math.floor(Math.random() * 100)
+      if (!args[0]) return embed(message).setError("Укажите что-то, или кого-то.😂").send()
+      let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase());
 
-        // let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase());
-        //
-        // if(!user) {
-        //   const newMsg = new MessageEmbed()
-        //   .setColor(cyan)
-        // }
+      if(!user) {
+        user = args.join(" ")
+        return embed(message).setPrimary(`**Твой совместимость с _${user}._**\n${progressBar(rand, 100, 10)}`).send()
+      }
+      return embed(message).setPrimary(`**Любовь между ${message.member} и ${user}**\n${progressBar(rand, 100, 10)}`).send()
 
-        // Assaign values to total and current values
-        // var total = 100;
-        // var current = 50;
-        // // First two arguments are mandatory
-        // message.channel.send(progressbar.splitBar(total, current, [10, ""]));
-        function progressBar(perc, ofMaxValue, size, line = '❤', slider = '🖤') {
-        if (!perc) throw new Error('Perc value is either not provided or invalid');
-        if (!current && current !== 0) throw new Error('Current value is either not provided or invalid');
-        if (isNaN(perc)) throw new Error('Perc value is not an integer');
-        if (isNaN(ofMaxValue)) throw new Error('ofMaxValue value is not an integer');
-        if (isNaN(size)) throw new Error('Size is not an integer');
-        const percentage = perc / ofMaxValue; // Calculate the percentage of the bar
-        const progress = Math.round((size * percentage)); // Calculate the number of square caracters to fill the progress side.
-        const emptyProgress = size - progress; // Calculate the number of dash caracters to fill the empty progress side.
 
-        const progressText = line.repeat(progress); // Repeat is creating a string with progress * caracters in it
-        const emptyProgressText = slider.repeat(emptyProgress); // Repeat is creating a string with empty progress * caracters in it
-        const percentageText = Math.round(percentage * 100) + '%'; // Displaying the percentage of the bar
-
-        const bar = '[' + progressText + emptyProgressText + ']' + percentageText + ''; // Creating the bar
-        message.channel.send(bar);
-        }
     }
 }
